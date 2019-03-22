124d1ced7ae2ca7aeea781803cd53cf3d9ae4cb2
Bharat Kathi <bharat1031@gmail.com>
2019-03-22 07:39:57 -0700

    Added App Drawer

---
+++ lib/screens/scouting/scouting_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ScoutingPage extends StatefulWidget {
+  @override
+  _ScoutingPageState createState() => _ScoutingPageState();
+}
+
+class _ScoutingPageState extends State<ScoutingPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
